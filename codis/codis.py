890863f5cd1272207247ca3e7ce2e7f3f9fefbaf
vipwangtian <32926504+vipwangtian@users.noreply.github.com>
2018-01-22 16:45:49 +0800

    Update codis.py

--- codis/codis.py
+++ codis/codis.py
@@ -8,7 +8,7 @@ import json
 import requests
 import subprocess
 
-from cmds import cmds
+from common.cmds import cmds
 from optparse import OptionParser
 from kazoo.client import KazooClient
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
