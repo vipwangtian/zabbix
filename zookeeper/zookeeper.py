1ee8fbc7c66438dda5673b879851fa674d75ebab
vipwangtian <vipwangtian@users.noreply.github.com>
2018-01-24 13:49:32 +0800

    use kazoo to send 4letter cmd

--- zookeeper/zookeeper.py
+++ zookeeper/zookeeper.py
@@ -6,87 +6,63 @@ import re
 import subprocess
 import os
 
+from kazoo.client import KazooClient
 from StringIO import StringIO
  
 class ZooKeeper(object):
  
-    def __init__(self, host='localhost', port='2181', timeout=1):
+    def __init__(self):
         self._address = (host, int(port))
-        self._timeout = timeout
         self._result  = {}
  
     def _send_cmd(self, cmd):
         """ Send a 4letter word command to the server """
-        s = socket.socket()
-        s.settimeout(self._timeout)
- 
-        s.connect(self._address)
-        s.send(cmd)
- 
-        data = s.recv(2048)
-        s.close()
- 
-        return data
+        zk = KazooClient(hosts = '127.0.0.1:2181')
+        zk.start()
+        ret = zk.command(cmd)
+        zk.stop()
+        return ret
  
     def get_stats(self):
         pid = self._get_pid()
-        """ Get ZooKeeper server stats as a map """
-        data_mntr = self._send_cmd('mntr')
-        data_ruok = self._send_cmd('ruok')
-        if data_mntr:
-            result_mntr = self._parse(data_mntr)
-        if data_ruok:
-            result_ruok = self._parse_ruok(data_ruok)
- 
-        self._result = dict(result_mntr.items() + result_ruok.items())
-        
-        if not self._result.has_key('zk_followers') and not self._result.has_key('zk_synced_followers') and not self._result.has_key('zk_pending_syncs'):
-            ##### the tree metrics only exposed on leader role zookeeper server, we just set the followers' to 0
-            leader_only = {'zk_followers':0,'zk_synced_followers':0,'zk_pending_syncs':0}    
-            self._result = dict(result_mntr.items() + result_ruok.items() + leader_only.items() )
-        self._send2zabbix()
+        if pid:
+            """ Get ZooKeeper server stats as a map """
+            data_mntr = self._send_cmd('mntr')
+            data_ruok = self._send_cmd('ruok')
+            if data_mntr:
+                result_mntr = self._parse(data_mntr)
+            if data_ruok:
+                result_ruok = self._parse_ruok(data_ruok)
+     
+            self._result = dict(result_mntr.items() + result_ruok.items())
+            
+            if not self._result.has_key('zk_followers') and not self._result.has_key('zk_synced_followers') and not self._result.has_key('zk_pending_syncs'):
+                ##### the tree metrics only exposed on leader role zookeeper server, we just set the followers' to 0
+                leader_only = {'zk_followers':0,'zk_synced_followers':0,'zk_pending_syncs':0}    
+                self._result = dict(result_mntr.items() + result_ruok.items() + leader_only.items() )
+            self._send2zabbix()
         return pid
  
     def _parse(self, data):
         """ Parse the output from the 'mntr' 4letter word command """
         h = StringIO(data)
-         
         result = {}
         for line in h.readlines():
             try:
-                key, value = self._parse_line(line)
-                result[key] = value
+                key, value = map(str.strip, line.split('\t'))
+                result[key] = int(value)
             except ValueError:
-                pass # ignore broken lines
- 
+                pass
         return result
  
     def _parse_ruok(self, data):
         """ Parse the output from the 'ruok' 4letter word command """
-        
         h = StringIO(data)
-        
         result = {}
-        
         ruok = h.readline()
         if ruok:
            result['zk_server_ruok'] = ruok
-  
         return result
-   
-    def _parse_line(self, line):
-        try:
-            key, value = map(str.strip, line.split('\t'))
-        except ValueError:
-            raise ValueError('Found invalid line: %s' % line)
- 
-        if not key:
-            raise ValueError('The key is mandatory and should not be empty')
-        try:
-            value = int(value)
-        except (TypeError, ValueError):
-            pass
-        return key, value
  
     def _get_pid(self):
         #  ps -ef|grep java|grep zookeeper|awk '{print $2}'
